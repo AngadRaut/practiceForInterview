cd8e4eeb8873757d88b93158d18f478da106d6c8
Angad Raut <anadraut6493@gmail.com>
2025-01-30 23:22:13 +0530

    all character count from string

--- interviewPractice/practiceForInterview/src/main/java/org/example/stringPractice/AllCharsCount.java
+++ interviewPractice/practiceForInterview/src/main/java/org/example/stringPractice/AllCharsCount.java
@@ -1,17 +1,28 @@
 package org.example.stringPractice;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class AllCharsCount {
     public static void main(String[] args) {
         String string = "you are $@best jus124t you^& need som*%!@e practice.";
+        // using simple for loop
+        int count = 0;
+        for(int i = 0 ; i < string.length() ; i++){
+            if(Character.isLetter(string.charAt(i))){
+                count++;
+            }
+        }
+        System.out.println(count);
+
         // using java 8
-
-
+        List<Character> list = new ArrayList<>();
+        for(int i = 0 ; i < string.length() ; i++){
+            list.add(string.charAt(i));
+        }
+        System.out.println(list);
+        long count1 = list.stream().filter(Character::isLetter).count();
+        System.out.println(count1);
 
     }
 }
